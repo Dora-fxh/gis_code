a77c093fda6b4db1cc3d8fd22552a6bf2d05d3ec
Dora--fxh <1499812616@qq.com>
2020-11-02 01:15:48 +0800

    revert

--- test1.R
+++ test1.R
@@ -1,13 +1,10 @@
-1+1
-2==2
 
-## out
-``` r
+install.packages("reprex")
+library(reprex)
 A <- 1
 B <- 2
 C <- A+B
-C
-#> [1] 3
-```
+C+A
+newChange=0
 
-<sup>Created on 2020-11-01 by the [reprex package](https://reprex.tidyverse.org) (v0.3.0)</sup>
+reprex()
