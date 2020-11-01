f58c22fab68bcfda3123375b3900a36a9a7197ac
Dora--fxh <1499812616@qq.com>
2020-11-01 20:59:29 +0800

    4

--- test1.R
+++ test1.R
@@ -1,13 +1,9 @@
-1+1
-2==2
 
-## out
-``` r
+install.packages("reprex")
+library(reprex)
 A <- 1
 B <- 2
 C <- A+B
 C
-#> [1] 3
-```
 
-<sup>Created on 2020-11-01 by the [reprex package](https://reprex.tidyverse.org) (v0.3.0)</sup>
+reprex()
